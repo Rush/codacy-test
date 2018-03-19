4c29bd300c60846023245a520b662131120f04f3
Damian Kaczmarek <rush@rushbase.net>
2018-03-19 17:23:50 -0500

    Check bug

--- index.js
+++ index.js
@@ -2,4 +2,7 @@ console.log('Hello world');
 const fs = require('fs');
 fs.readFile('package.json', (err, data) => {
   console.log('Data', data);
+  fs.readFile('package.json', (err, data) => {
+    console.log('Data', data);
+  });
 })
