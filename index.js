a69da9c101e266d1a396073087b428c8f7d1d1b3
Damian Kaczmarek <rush@rushbase.net>
2018-03-19 17:26:10 -0500

    Test

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ console.log('Hello world');
 const fs = require('fs');
 fs.readFile('package.json', (err, data) => {
   console.log('Data', data);
-  fs.readFile('package.json', (err, data) => {
+  fs.readFile('package.json', (err2, data) => {
     console.log('Data', data);
   });
 })
